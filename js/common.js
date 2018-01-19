07eae58fa0e3566f9f60d0fbcfb30ed3f2c518fb
小东宁 <570944890@qq.com>
2018-01-19 23:11:43 +0800

    存放htmlml

---
+++ js/common.js
@@ -0,0 +1,29 @@
+$(function () {
+    var bar = $('.menu-bar');
+    var timer = null;
+    var timeEnd = null;
+    $('#menu li').mouseenter(function () { 
+        var othis = $(this);
+        bar.css({
+            left: othis.position().left + parseFloat(othis.css('marginLeft'))
+            , top: othis.position().top + othis.height() - bar.height()
+        });
+        timer = setTimeout(function () { 
+            bar.css({
+                width: othis.width()
+                ,opacity: 1
+            });
+        }, 100);
+        clearTimeout(timeEnd);
+    }).mouseleave(function () { 
+        var othis = $(this);
+        clearTimeout(timer);
+        timeEnd = setTimeout(function () { 
+            bar.css({
+                width: 0
+                ,left: bar.position().left + bar.width() / 2 
+                ,opacity: 0
+            });
+        },100);
+    });
+});
